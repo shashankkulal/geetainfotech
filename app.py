b2fa11e04e19e51efca53e1ca09f5232c9877b69
Shashank Suresh Kulal <kulal.shashank50@gmail.com>
2021-05-10 10:29:32 +0530

    Broken, Mixed and All
    May10_Morning

--- app.py
+++ app.py
@@ -5,100 +5,141 @@ from Geetainfotech import *
 app = Flask(__name__)
 key = "548d22495f4bbd2bd8cae8c2b804ffa4"
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/getTitle')
+@app.route("/getTitle")
 def title():
-	token = request.args.get('token')
-	page = request.args.get('page_url')
+	token = request.args.get("token")
+	page = request.args.get("page_url")
 	if token == key:
 		if not validators.url(page):
-			return "Invalide Page URL"
+			return {"errors": 1, "status": 400, "result": "Invalid Url"}
 		else:
 			t = getTitle(page)
-			t['status'] = 200
 			return jsonify(t)
-	return "Unauthorized"
+	return {"errors": 1, "status": 401, "result": "Un-Authorized"}
 
-@app.route('/getDescription')
+@app.route("/getDescription")
 def description():
-	token = request.args.get('token')
-	page = request.args.get('page_url')
+	token = request.args.get("token")
+	page = request.args.get("page_url")
 	if token == key:
 		if not validators.url(page):
-			return "Invalide Page URL"
+			{"errors": 1, "status": 400, "result": "Invalid Url"}
 		else:
 			d = getDescription(page)
-			d['status'] = 200
 			return jsonify(d)
-	return "Unauthorized"
+	return {"errors": 1, "status": 401, "result": "Un-Authorized"}
 
-@app.route('/getHeading')
+@app.route("/getHeading")
 def heading():
-	token = request.args.get('token')
-	page = request.args.get('page_url')
+	token = request.args.get("token")
+	page = request.args.get("page_url")
 	if token == key:
 		if not validators.url(page):
-			return "Invalide Page URL"
+			return {"errors": 1, "status": 400, "result": "Invalid Url"}
 		else:
 			d = getHeading(page)
-			d['status'] = 200
 			return jsonify(d)
-	return "Unauthorized"
+	return {"errors": 1, "status": 401, "result": "Un-Authorized"}
 
-@app.route('/getRobots')
+@app.route("/getRobots")
 def robots():
-	token = request.args.get('token')
-	page = request.args.get('page_url')
+	token = request.args.get("token")
+	page = request.args.get("page_url")
 	if token == key:
 		if not validators.url(page):
-			return "Invalide Page URL"
+			return {"errors": 1, "status": 400, "result": "Invalid Url"}
 		else:
 			d = getRobots(page)
-			d['status'] = 200
 			return jsonify(d)
-	return "Unauthorized"
+	return {"errors": 1, "status": 401, "result": "Un-Authorized"}
 
-@app.route('/getSitemap')
+@app.route("/getSitemap")
 def sitemap():
-	token = request.args.get('token')
-	page = request.args.get('page_url')
+	token = request.args.get("token")
+	page = request.args.get("page_url")
 	if token == key:
 		if not validators.url(page):
-			return "Invalide Page URL"
+			return {"errors": 1, "status": 400, "result": "Invalid Url"}
 		else:
 			d = getSitemap(page)
-			d['status'] = 200
 			return jsonify(d)
-	return "Unauthorized"
+	return {"errors": 1, "status": 401, "result": "Un-Authorized"}
 
-@app.route('/getAlt')
+@app.route("/getAlt")
 def alt():
-	token = request.args.get('token')
-	page = request.args.get('page_url')
+	token = request.args.get("token")
+	page = request.args.get("page_url")
 	if token == key:
 		if not validators.url(page):
-			return "Invalide Page URL"
+			return {"errors": 1, "status": 400, "result": "Invalid Url"}
 		else:
 			d = getAlt(page)
-			d['status'] = 200
 			return jsonify(d)
-	return "Unauthorized"
+	return {"errors": 1, "status": 401, "result": "Un-Authorized"}
 
-@app.route('/getCanonical')
+@app.route("/getCanonical")
 def canonical():
-	token = request.args.get('token')
-	page = request.args.get('page_url')
+	token = request.args.get("token")
+	page = request.args.get("page_url")
 	if token == key:
 		if not validators.url(page):
-			return "Invalide Page URL"
+			return {"errors": 1, "status": 400, "result": "Invalid Url"}
 		else:
 			d = getCanonical(page)
-			d['status'] = 200
 			return jsonify(d)
-	return "Unauthorized"
+	return {"errors": 1, "status": 401, "result": "Un-Authorized"}
 
-if __name__ == '__main__':
+@app.route("/getHttpStatus")
+def httpc():
+	token = request.args.get("token")
+	page = request.args.get("page_url")
+	if token == key:
+		if not validators.url(page):
+			return {"errors": 1, "status": 400, "result": "Invalid Url"}
+		else:
+			d = getHttpStatus(page)
+			return jsonify(d)
+	return {"errors": 1, "status": 401, "result": "Un-Authorized"}
+
+@app.route("/getMixedContent")
+def mix():
+	token = request.args.get("token")
+	page = request.args.get("page_url")
+	if token == key:
+		if not validators.url(page):
+			return {"Errors" : "Invalid Page Url"}
+		else:
+			d = getMixedContent(page)
+			return jsonify(d)
+	return {"Errors": "Unauthorized"}
+
+@app.route("/getBroken")
+def broken():
+	token = request.args.get("token")
+	page = request.args.get("page_url")
+	if token == key:
+		if not validators.url(page):
+			return {"errors": 1, "status": 400, "result": "Invalid Url"}
+		else:
+			d = getBroken(page)
+			return jsonify(d)
+	return {"errors": 1, "status": 401, "result": "Un-Authorized"}
+
+@app.route("/getAll")
+def gall():
+	token = request.args.get("token")
+	page = request.args.get("page_url")
+	if token == key:
+		if not validators.url(page):
+			return {"errors": 1, "status": 400, "result": "Invalid Url"}
+		else:
+			d = getAll(page)
+			return jsonify(d)
+	return {"errors": 1, "status": 401, "result": "Un-Authorized"}
+
+if __name__ == "__main__":
     app.run(debug = True)
